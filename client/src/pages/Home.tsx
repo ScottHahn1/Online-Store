01199b136b9246baf3b9b0b922d4b52732e0e3d1
Scott Hahn <scotthahn2210@gmail.com>
2024-05-26 12:35:30 +0200

    removed comments

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -16,20 +16,13 @@ const Home = ({ blur, setCategory }: Props) => {
       <div className="banner" style={{ backgroundImage: `url(${image})` }}>
         <div className="banner-text">
           <h1>
-            {/* Lorem Ipsum dolor sit amet, consectetur <br></br>adipiscing elit. */}
             Stay ahead of the competition, <br></br> at an affordable price.
           </h1>
 
           <p>
-            {/* Quisque condimentum tempor cursus. Nulla nunc massa, porta a
-            venenatis in, ornare at arcu. Nam pharetra, dolor id suscipit
-            semper, nisl tellus lacinia ligula, et sollicitudin nibh lorem eget
-            metus. Donec in aliquam tellus. */}
-            {/* Find what you're looking for. */}
             Tired of losing? Tired of not being able to rank up? Look no further than Aggro Gaming,
             your one stop shop in order to become the gamer you know you can be. From headsets to controllers to keyboards and more,
             whatever gaming equipment you need, we'll provide at competitve prices. Stay equipped, stay aggressive, stay winning!
-            {/* Remember, if you're winning at the game, you're winning in life! */}
           </p>
 
           <Link to="/products">
