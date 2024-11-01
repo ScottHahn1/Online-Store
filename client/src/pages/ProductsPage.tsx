537bd7edae22694b93ac7e3b093d78625adca1d0
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-01 17:04:58 +0200

    changed pagination from keyset to offset/limit

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "../hooks/Api";
 import { ChangeEvent, Dispatch, SetStateAction, useEffect, useLayoutEffect, useState } from "react";
 import "../styles/Products.css";
-import useCategory from "../hooks/useCategory";
-import Products from "../components/Products";
+import ProductCard from "../components/ProductCard";
+import axios from "axios";
+import "../styles/Products.css";
+import Categories from "../components/Categories";
 
 type Data = {
   brand: string;
@@ -19,14 +20,7 @@ type Data = {
   ratingCount: number;
   title: string;
   overview: string;
-}[];
-
-type Query = {
-  column: string
-  lastSeen: number | string | null
-  lastSeenId: number | string | null
-  prev: boolean | null
-}
+};
 
 type Props = {
   category: string
@@ -34,108 +28,97 @@ type Props = {
   blur: boolean
 }
 
+type QueryParams = {
+  category: string;
+  page: number;
+  sortBy: string;
+}
+
+const getProducts = async <T, >(url: string, params: T) => {
+  const response = await axios.get(url, {
+    params: params
+  });
+  return response.data;
+}
+
 const ProductsPage = ({ category, setCategory, blur }: Props) => {
-  const [lastSeenProduct, setLastSeenProduct] = useState<number | string | null>(null);
-  const [lastSeenId, setLastSeenId] = useState<number | null>(null);
-  const [page, setPage] = useState(1);
-  const [sortBy, setSortBy] = useState("price");
-  const [products, setProducts] = useState<Data>([]);
-  const [prev, setPrev] = useState<boolean | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortBy, setSortBy] = useState('price');
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["allProducts", { lastSeenProduct, sortBy, page, category }],
-    queryFn: () =>
-      getProducts<Data, Query>(`https://online-store-backend-zeta.vercel.app/products`, { column: sortBy, lastSeen: lastSeenProduct, lastSeenId: lastSeenId , prev: prev }),
-    enabled: !category,
-  });
-
-  const { data: categoryData } = useCategory(category, sortBy, lastSeenProduct, lastSeenId, prev);
-
-  useEffect(() => {
-    if (data && prev) {
-      setPrev(null);
-      setProducts(data.reverse());
-    } else if (data && !prev) {
-      setProducts(data);
-    }
-  }, [data])
-
-  useEffect(() => {
-    if (categoryData && prev) {
-      setPrev(null);
-      setProducts(categoryData.reverse());
-    } else if (categoryData && !prev) {
-      setProducts(categoryData);
-    }
-  }, [categoryData])
-
-  useEffect(() => {
-    page === 1 && setLastSeenProduct(null);
-  }, [page])
-
-  useEffect(() => {
-    setPage(1);
-  }, [sortBy, category])
-
-  const handlePreviousClick = () => {
-    page > 1 && setPrev(true);
-    if (products) {
-      if (sortBy === "price") {
-        setLastSeenProduct(products[0].price);
-      } else if (sortBy === "rating") {
-        setLastSeenProduct(products[0].rating);
-      } else {
-        setLastSeenProduct(products[0].title);
-      }
-    }
-    setPage(prev => prev - 1);
-  }
-
-  const handleNextClick = () => {
-    prev && setPrev(null);
-
-    switch (sortBy) {
-      case "price":
-        products && setLastSeenProduct(products[products.length - 1].price);
-        products && setLastSeenId(products[products.length - 1].productId);
-        break;
-
-      case "title":
-        products && setLastSeenProduct(products[products.length - 1].title);
-        products && setLastSeenId(products[products.length - 1].productId);
-        break;
-
-      case "rating":
-        products && setLastSeenProduct(products[products.length - 1].rating);
-        products && setLastSeenId(products[products.length - 1].productId);
-        break;
-    }
-    setPage(prev => prev + 1);
-  }
-
   const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
     setSortBy(e.currentTarget.value);
-    setLastSeenProduct(null);
+    setCurrentPage(1); // changing the sortBy value will set the current page back to page 1
   }
-  
+
+  //get the total amount of products in a category
+  const { data: totalProducts } = useQuery({
+    queryKey: ['totalProducts', category],
+    queryFn: () => getProducts<{ category: string }>(`http://localhost:8888/products/total`, { category: category })
+  })
+
+  //get all products in a category
+  const { data: products, isLoading } = useQuery<Data[]>({
+    queryKey: ['allProducts', category, currentPage, sortBy],
+    queryFn: () => getProducts<QueryParams>('http://localhost:8888/products', { category: category, page: currentPage, sortBy: sortBy }),
+  });
+
+  useEffect(() => {
+    setCurrentPage(1); // changing the category will set the current page back to page 1
+  }, [category])
+
   return (
-    <>
+    <div className='flex-space-between'>
+      <Categories clickedCategory={category} setClickedCategory={setCategory} />
       {
-        isLoading ? <div className="loading"></div>
-        :
-        <Products 
-          blur={blur}
-          category={category} setCategory={setCategory} 
-          products={products} 
-          handleSelect={handleSelect} handlePreviousClick={handlePreviousClick} handleNextClick={handleNextClick} 
-          page={page} 
-        />
+        !isLoading &&
+        <div className='products-container'>
+          <div className='products-header'>
+            { category ? <h2>{category}</h2> : <h2>All Products</h2> }
+            { totalProducts && <div>{totalProducts.count} items</div> }
+            <select onChange={handleSelect} style={{ fontSize: "1.2rem" }}>
+              <option value='price'>Price</option>
+              <option value='title'>A-Z</option>
+              <option value='rating'>Rating</option>
+            </select>
+          </div>
+          <div className='cards'>
+            { products?.map(product => <ProductCard product={product} />) }
+          </div>
+          <div className="pages">
+            {
+              currentPage > 1 &&
+              <button onClick={() => {
+                setCurrentPage((prev) => prev - 1)
+              }}>
+                <p>Previous</p>
+              </button>
+            }
+
+            {
+              totalProducts && 
+              <div>
+                Page: {currentPage} of {Math.ceil(totalProducts.count / 50)}
+              </div>
+            }
+
+            {
+              products && products.length > 49 && 
+              <button onClick={() => setCurrentPage((prev) => prev + 1)}>
+                <p>Next</p>
+              </button>
+            }
+          </div>
+        </div>
       }
-    </>
+
+      {
+        isLoading && <div className='loading'></div>
+      }
+    </div>
   );
 };
 
