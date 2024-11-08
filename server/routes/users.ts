66b21ca7f2f91af28baa34c16fe65a57c11a4824
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-08 22:46:19 +0200

    Stored JWT in cookie

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -34,7 +34,7 @@ usersRouter.post('/register', async (req, res) => {
 usersRouter.post('/login', (req, res) => {
     const sql = 'SELECT * FROM users WHERE username = ?';
     const { username, password } = req.body;
-    
+
     pool.query(sql, [username], (err: any, result: any) => {
         if (err) {
             return res.status(500).json({ message: 'Internal server error!' })
@@ -43,13 +43,20 @@ usersRouter.post('/login', (req, res) => {
         if (result.length > 0) {
             bcrypt.compare(password, result[0].password, (err: any, response: any) => {
                 if (err) {
-                    return res.status(200).json('Error logging in');
+                    return res.status(500).json('Error logging in');
                 }
 
                 if (response) {
                     const userId = result[0].userId;
-                    const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: 3600 });
-                    return res.status(200).json({ success: true, login: true, token, userId, username });
+                    const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '1h' });
+                    res.cookie('authToken', token, 
+                        { 
+                            httpOnly: true, 
+                            secure: process.env.COOKIE_SECURE === 'true',
+                            sameSite: 'strict'
+                        }
+                    );
+                    return res.status(200).json({ success: true, login: true, message: 'Login Successful!', token, userId, username });
                 } else {
                     return res.status(401).json({ success: false, message: 'Invalid username or password' });
                 }
