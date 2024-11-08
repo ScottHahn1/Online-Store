7265b16c2e5d0acb4688bd29497b3769150d9da9
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-08 19:55:39 +0200

    installed cookieParser

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import usersRouter from "./routes/users";
 import cartRouter from "./routes/cart";
 import categoriesRouter from "./routes/categories";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 8888;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin: ["https://online-store-frontend-mocha.vercel.app", FRONTEND_URL],
     methods: ["GET", "POST", "DELETE"],
