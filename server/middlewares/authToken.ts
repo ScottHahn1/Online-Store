b443ebae6e021373041b8d75bf6928fe105a29c7
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-12 22:44:19 +0200

    Added middleware to verify JWT token from cookie

---
+++ server/middlewares/authToken.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
+interface CustomRequest extends Request {
+    user?: {
+        userId: number;
+        username: string;
+    }
+}
+
+const authenticateToken = (req: CustomRequest, res: Response, next: NextFunction) => {
+    const token = req.cookies.authToken;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication required. Please provide a valid token.' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err: any, user: any) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invaild token.' })
+        }
+
+        if (user) {
+            req.user = user; 
+            next();
+        }
+    })
+}
+
+export default authenticateToken;
