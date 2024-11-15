7227c388d4df15aab2909dc62daa429a2cb9bb3a
ScottHahn1 <scotthahn2210@gmail.com>
2024-11-15 12:51:25 +0200

    Replaced user logged in status from localstorage to state

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { getLocalStorage } from "../utils/LocalStorage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { HashLink } from "react-router-hash-link";
 import Logout from "./Logout";
 
 type Props = {
-  loggedIn: string;
-  setLoggedIn: Dispatch<SetStateAction<string>>;
+  loggedIn: boolean | null;
   setShowCart: Dispatch<SetStateAction<boolean>>;
   blur: boolean;
 };
 
-const Navbar = ({ setShowCart, blur, loggedIn, setLoggedIn }: Props) => {
+const Navbar = ({ loggedIn, setShowCart, blur }: Props) => {
   const [showPopup, setShowPopup] = useState(false);
 
   const handleCartClick = () => {
-    getLocalStorage("token") && setShowCart(true);
+    loggedIn && setShowCart(true);
     showPopup && setShowPopup(false);
   };
 
@@ -27,63 +25,61 @@ const Navbar = ({ setShowCart, blur, loggedIn, setLoggedIn }: Props) => {
   }, [showPopup]);
 
   return (
-    <nav style={{ filter: blur ? "blur(3px)" : "" }}>
+    <nav style={{ filter: blur ? 'blur(3px)' : '' }}>
       <HashLink to="/#" smooth={true}>
-        <h1 style={{ cursor: "pointer", textDecoration: "none" }}>
-          <span style={{ color: "whitesmoke" }}>AGGRO</span>{" "}
-          <span style={{ color: "red" }}>GAMING</span>
+        <h1 style={{ cursor: 'pointer', textDecoration: 'none' }}>
+          <span style={{ color: 'whitesmoke' }}>AGGRO</span>{' '}
+          <span style={{ color: 'red' }}>GAMING</span>
         </h1>
       </HashLink>
 
       <ul>
-        <HashLink to="/#" smooth={true}>
+        <HashLink to='/#' smooth={true}>
           <li>HOME</li>
         </HashLink>
 
-        <Link to="/products">
+        <Link to='/products'>
           <li>PRODUCTS</li>
         </Link>
 
         <HashLink
-          to="/#home-categories"
+          to='/#home-categories'
           smooth={true}
           scroll={(el) => {
             const yOffset = -100;
             const y = el.getBoundingClientRect().top + yOffset;
-            window.scrollTo({ top: y, behavior: "smooth" });
+            window.scrollTo({ top: y, behavior: 'smooth' });
           }}
         >
           <li>CATEGORIES</li>
         </HashLink>
       </ul>
 
-      <div className="login-register">
-        {!loggedIn ? (
-          <Link to="/login">
+      <div className='login-register'>
+        {loggedIn ? (
+          <Logout />
+        ) : (
+          <Link to='/login'>
             <button>Login</button>
           </Link>
-        ) : (
-          <Logout setLoggedIn={setLoggedIn} />
         )}
 
-        <Link to="/register">
+        <Link to='/register'>
           <button>Register</button>
         </Link>
       </div>
 
       <FontAwesomeIcon
         icon={faShoppingCart}
-        color="white"
-        size="2x"
-        cursor="pointer"
-        onClick={() =>
-          getLocalStorage("userId") ? handleCartClick() : setShowPopup(true)
-        }
+        color='white'
+        size='2x'
+        cursor='pointer'
+        onClick={() => loggedIn ? handleCartClick() : setShowPopup(true)}
       />
 
       {showPopup && (
-        <div className="login-popup">
-          <span className="login-popup-text">Login required!</span>
+        <div className='login-popup'>
+          <span className='login-popup-text'>Login required!</span>
         </div>
       )}
     </nav>
