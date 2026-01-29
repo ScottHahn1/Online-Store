eaaf8c58c9bad89b0e9a9c58f0811424a9f1bd5c
ScottHahn1 <scotthahn2210@gmail.com>
2026-01-29 16:12:10 +0200

    feat: add email and accessToken to UserContext

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -2,13 +2,16 @@ import { Dispatch, ReactNode, SetStateAction, useContext, useState } from "react
 import { createContext } from "react";
 
 export type User = {
+    email: string | null;
     userId: number | null;
     username: string | null;
 }
     
 export type UserContextType = {
     user: User | null;
-    setUser: Dispatch<SetStateAction<User | null>>
+    setUser: Dispatch<SetStateAction<User | null>>;
+    accessToken: string | null;
+    setAccessToken: Dispatch<SetStateAction<string | null>>;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
@@ -27,9 +30,10 @@ export function useUserContext(): UserContextType {
 
 export const UserProvider = ({ children }: UserProviderProps) => {
     const [user, setUser] = useState<User | null>(null);
+    const [accessToken, setAccessToken] = useState<string | null>(null);
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, accessToken, setAccessToken }}>
           {children}
         </UserContext.Provider>
       );
