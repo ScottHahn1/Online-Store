0b7596ab274b8715f82775cade4af17d51235391
ScottHahn1 <scotthahn2210@gmail.com>
2026-02-01 17:44:33 +0200

    chore: declare @paystack/inline-js module

--- client/src/declarations.d.ts
+++ client/src/declarations.d.ts
@@ -1,2 +1,20 @@
 declare module '*.jpg';
 declare module '*.png';
+
+declare module '@paystack/inline-js' {
+  interface PaystackInlineOptions {
+    key: string;
+    email: string;
+    amount: number;
+    ref?: string;
+    metadata?: Record<string, any>;
+    callback: (response: { reference: string }) => void;
+    onClose?: () => void;
+  }
+
+  export default class PaystackPop {
+    constructor(options?: PaystackInlineOptions);
+    openIframe(): void;
+    resumeTransaction(access_code: string): void;
+  }
+}
