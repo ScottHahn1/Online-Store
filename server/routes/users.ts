4d290eec883378b03c20292f6b5c23cd58e69e60
ScottHahn1 <scotthahn2210@gmail.com>
2026-03-01 17:38:35 +0200

    fix: set cookie path to root

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -53,7 +53,8 @@ usersRouter.post('/login', async (req, res) => {
                         httpOnly: true,
                         secure: process.env.NODE_ENV === "production",
                         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-                        maxAge: 7 * 24 * 60 * 60 * 1000
+                        maxAge: 7 * 24 * 60 * 60 * 1000,
+                        path: "/",
                     }
                 );
 
