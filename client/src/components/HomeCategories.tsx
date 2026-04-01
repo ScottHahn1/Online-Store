20d5df7ebeba1c5adb767340666373847f341060
ScottHahn1 <scotthahn2210@gmail.com>
2026-04-01 20:14:04 +0200

    refactor: use category slug in home category navigation

--- client/src/components/HomeCategories.tsx
+++ client/src/components/HomeCategories.tsx
@@ -6,25 +6,30 @@ const HomeCategories = () => {
   const { data, isLoading } = useCategories();
   const navigate = useNavigate();
 
-    return (
-      <div>
-        {!isLoading && (
-          <div className="home-categories" id="home-categories">
-            <h2 style={{ width: "100%", textAlign: "center" }}>Categories</h2>
+  const slugify = (categoryName: string) => {
+    return categoryName?.toLowerCase().replaceAll("&", "and").replaceAll(" ", "-")
+  } 
 
-            {data?.map(category => (
-                <div className="home-category" key={category.id} onClick={() => {
-                  setCategory(category.id)
-                  navigate('/products')
-                }}>
-                  <img src={category.image} alt={category.name} />
-                  <p>{category.name}</p>
-                </div>
-            ))}
-          </div>
-        )}
-      </div>
-    )
+  return (
+    <div>
+      {!isLoading && (
+        <div className="home-categories" id="home-categories">
+          <h2 style={{ width: "100%", textAlign: "center" }}>
+            Categories
+          </h2>
+
+          {data?.map(category => (
+              <div className="home-category" key={category.id} onClick={() => {
+                navigate(`/products/?category=${slugify(category.name)}`)
+              }}>
+                <img src={category.image} alt={category.name} />
+                <p>{category.name}</p>
+              </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default HomeCategories;
